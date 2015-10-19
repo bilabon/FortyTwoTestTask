a5f7cd74224af2827408a3ffda68844349faa30b
Dmytro Bazas <babyx64@gmail.com>
2015-10-19 22:37:49 +0300

    ticket:1 added logic(app, view, models)

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class HomeView(DetailView):
+    model = User
+    template_name = 'home.html'
+
+    def get_object(self, queryset=None):
+        obj = User.objects.first()
+        return obj
--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'south',
+    'selenium',
+
     'apps.hello',
     'apps.contact',
 )
