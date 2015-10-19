e0567890ff0956b65c90c724552fa3ca3bd08d55
Dmytro Bazas <babyx64@gmail.com>
2015-10-19 22:35:29 +0300

    ticket:1 fixed test for implementation

--- apps/contact/tests.py
+++ apps/contact/tests.py
@@ -3,19 +3,24 @@ from django.core.urlresolvers import reverse
 
 
 class ContactPageTest(TestCase):
+    fixtures = ['fixtures/user.json']
 
     def test_exist_main_url(self):
         """
         Check main URL responce.
         """
-        response = self.client.get(reverse('main'))
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
     def test_assert_data_at_main_url(self):
         """
-        Check present my name, surname, date of birth, bio, contacts
-        on the main page.
+        Check present my name, surname, date of birth, email, bio, contacts
+        on the home page.
         """
-        response = self.client.get(reverse('main'))
-        self.assertEqual(response.content, 'Dmytro')
-        self.assertEqual(response.content, 'Bazas')
+        response = self.client.get(reverse('home'))
+        self.assertIn('Name: </span>John', response.content)
+        self.assertIn('Surname: </span>Smith', response.content)
+        self.assertIn('Date of birth: </span>10/15/2015', response.content)
+        self.assertIn('Email: </span>test@example.com', response.content)
+        self.assertIn('Bio: </span>some bio', response.content)
+        self.assertIn('Contacts: </span>some contacts', response.content)
