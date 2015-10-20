d46bd4326c09acfdd4e743b22a251b298a32c244
Dmytro Bazas <babyx64@gmail.com>
2015-10-20 14:48:01 +0300

    added all implementation

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import DetailView
+import json
+from django.http import HttpResponse, Http404
+from django.views.generic import View, DetailView, ListView
 from django.contrib.auth.models import User
+
+from .models import RequestLog
 
 
 class HomeView(DetailView):
@@ -12,3 +16,31 @@ class HomeView(DetailView):
     def get_object(self, queryset=None):
         obj = User.objects.first()
         return obj
+
+
+class RequestLogListView(ListView):
+    """
+    Render page with 10 http requests and return http request count.
+    """
+    model = RequestLog
+    paginate_by = 10
+    template_name = 'request-log.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RequestLogListView, self).get_context_data(**kwargs)
+        context['request_count'] = RequestLog.objects.count()
+        return context
+
+
+class RequestCountView(View):
+    """Return count of http requests"""
+
+    def get(self, request, *args, **kwargs):
+        if request.is_ajax():
+            context = {}
+            mimetype = 'application/json'
+            context['request_count'] = RequestLog.objects.count()
+            data = json.dumps(context)
+            return HttpResponse(data, mimetype)
+        else:
+            raise Http404
