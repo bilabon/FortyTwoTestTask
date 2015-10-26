f80434459bb77ddaea50927a5dc555a4f763d9d1
Dmytro Bazas <babyx64@gmail.com>
2015-10-26 04:40:05 +0200

    task:8 added test for django command

--- apps/contact/tests.py
+++ apps/contact/tests.py
@@ -1,5 +1,11 @@
 import os
 import json
+from StringIO import StringIO
+
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.contrib.contenttypes.models import ContentType
+
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
@@ -180,3 +186,40 @@ class SignalProcessorTest(TestCase):
             object_name='Contact',
             action=ObjectLogEntry.DELETE)
         self.assertEqual(obj.count(), 1)
+
+
+class CommandTests(TestCase):
+    '''
+    Test infomodels command that prints all project models and
+    the count of objects in every model.
+    '''
+
+    def test_command_infomodels_stdout(self):
+        '''
+        Testing stdout of command.
+        '''
+        stdout = StringIO()
+        call_command('infomodels', stdout=stdout)
+        for ct in ContentType.objects.all():
+            m = ct.model_class()
+            line = "%s.%s\t%d" % (
+                m.__module__, m.__name__, m._default_manager.count())
+            self.assertIn(line, stdout.getvalue())
+
+    def test_command_infomodels_stderr(self):
+        '''
+        Testing stderr of command.
+        '''
+        stderr = StringIO()
+        call_command('infomodels', stderr=stderr)
+        for ct in ContentType.objects.all():
+            m = ct.model_class()
+            line = "%s.%s\t%d" % (
+                m.__module__, m.__name__, m._default_manager.count())
+            self.assertIn('Error: %s' % line, stderr.getvalue())
+
+    def test_exeption_not_allow_any_args(self):
+        '''
+        Raise CommandError: The command does not allow any args
+        '''
+        self.assertRaises(CommandError, call_command, 'infomodels args')
