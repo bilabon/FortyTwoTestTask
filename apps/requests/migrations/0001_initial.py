d938716693509f7932af7631d062836295767529
Dmytro Bazas <babyx64@gmail.com>
2015-10-30 15:46:32 +0200

    added initial user

--- apps/requests/migrations/0001_initial.py
+++ apps/requests/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Migration(SchemaMigration):
@@ -17,6 +18,13 @@ class Migration(SchemaMigration):
             ('timestamp', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
         ))
         db.send_create_signal(u'requests', ['RequestLog'])
+
+        #create initial user
+
+        user = User.objects.create_user('user', 'babyx64@gmail.com', 'user')
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
 
 
     def backwards(self, orm):
