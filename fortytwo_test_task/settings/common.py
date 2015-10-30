86b4056f671c92bee7c5c40b2f8e500b02069a7c
Dmytro Bazas <babyx64@gmail.com>
2015-10-30 15:50:01 +0200

    chenged db name

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'fortytwo_test_task.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'project_db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'sqlite.sqlite3'),
     }
 }
 
