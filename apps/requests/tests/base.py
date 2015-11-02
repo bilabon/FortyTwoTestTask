256aac4a0a558fa0f6bc472b69126e9e7a01f164
Dmytro Bazas <babyx64@gmail.com>
2015-11-02 14:41:26 +0200

    ticket 3: added factory-boy and mock to tests

--- apps/requests/tests/base.py
+++ apps/requests/tests/base.py
@@ -1,15 +1,15 @@
 import os
-# from mock import patch
+from mock import patch
 from django.conf import settings
 from django.test import TestCase
 
 
-# class FakeMiddleware(object):
-#     '''
-#     Fake empty middleware
-#     '''
-#     def process_request(self, request):
-#         return None
+class FakeMiddleware(object):
+    '''
+    Fake empty middleware
+    '''
+    def process_request(self, request):
+        return None
 
 
 class BaseSetup(TestCase):
@@ -20,9 +20,9 @@ class BaseSetup(TestCase):
 
     def setUp(self):
         settings.MEDIA_ROOT = os.path.join(settings.BASE_DIR, 'fixtures')
-    #     self.patcher1 = patch(
-    #         'apps.requests.middleware.SaveRequestMiddleware', FakeMiddleware)
-    #     self.patcher1.start()
+        self.patcher1 = patch(
+            'apps.requests.middleware.SaveRequestMiddleware', FakeMiddleware)
+        self.patcher1.start()
 
-    # def tearDown(self):
-    #     self.patcher1.stop()
+    def tearDown(self):
+        self.patcher1.stop()
